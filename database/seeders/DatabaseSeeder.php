95c9ff11a6292a09eac0d1bd5b5ef2697d4de8b3
Rey <reancirl@codesquirrel.com.au>
2025-05-16 18:09:43 +0800

    seeder + role field

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Create admin user
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'CrateOS Admin',
+            'email' => 'admin@crateos.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin'
         ]);
     }
 }

a3affcfb1ced630013a67e46c5560c2a9d4775de
Rey <reancirl@codesquirrel.com.au>
2025-06-20 11:40:35 +0800

    feature/share event button

--- resources/views/events/components/car-registrants.blade.php
+++ resources/views/events/components/car-registrants.blade.php
@@ -21,12 +21,38 @@
                     Get started by sharing the event link with potential participants.
                 </p>
                 <div class="mt-6">
-                    <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                    <button 
+                        type="button" 
+                        onclick="copyEventUrl('{{ route('public.events.show', $event) }}')" 
+                        class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                        id="shareButton"
+                        data-tooltip-target="tooltip-share"
+                        data-tooltip-trigger="click"
+                    >
                         <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
+                            <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
                         </svg>
                         Share Event
                     </button>
+                    <div id="tooltip-share" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                        Link copied!
+                        <div class="tooltip-arrow" data-popper-arrow></div>
+                    </div>
+                    <script>
+                        function copyEventUrl(url) {
+                            navigator.clipboard.writeText(url).then(() => {
+                                const tooltip = document.getElementById('tooltip-share');
+                                tooltip.classList.remove('invisible', 'opacity-0');
+                                tooltip.classList.add('opacity-100');
+                                setTimeout(() => {
+                                    tooltip.classList.add('opacity-0');
+                                    setTimeout(() => tooltip.classList.add('invisible'), 150);
+                                }, 2000);
+                            }).catch(err => {
+                                console.error('Failed to copy URL: ', err);
+                            });
+                        }
+                    </script>
                 </div>
             </div>
         @else

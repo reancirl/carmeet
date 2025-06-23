64bbc3ffbeb0497e0a8e6b72c2b959508972622c
Rey <reancirl@codesquirrel.com.au>
2025-06-23 17:30:41 +0800

    try\fix users route issue

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        $this->middleware('admin');
+        $this->middleware(AdminMiddleware::class);
     }
 
     /**
